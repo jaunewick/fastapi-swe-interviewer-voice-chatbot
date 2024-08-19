65a1b810ff49b824acd733a369876bf7386c3d32
Daniel Giao <daniel.giao@icloud.com>
2024-08-19 12:00:49 -0400

    llm changed to SmolLM for performance

--- main.py
+++ main.py
@@ -12,25 +12,20 @@ async def root():
 @app.post("/audio")
 async def post_audio(file: UploadFile):
     user_message = speech2text(file)
-    bot_response = get_chat_response(user_message)
-    print(bot_response)
+    bot_response = get_bot_response(user_message)
 
 def speech2text(file):
     audio_file = open(file.filename, "rb")
-    whisper = pipeline('automatic-speech-recognition', model='openai/whisper-medium')
+    whisper = pipeline('automatic-speech-recognition', model='openai/whisper-medium', device=0)
     message = whisper(str(audio_file.name))
-    print(message)
-    return {"role": "user", "content": message}
+    return {"role": "user", "content":message['text']}
 
-def get_chat_response(user_message):
+def get_bot_response(user_message):
     messages = load_messages()
     messages.append(user_message)
-    pipe = pipeline("text-generation", model="mistralai/Mixtral-8x7B-Instruct-v0.1")
-    bot_response = pipe(messages)
-    print(bot_response[0]['generated_text'])
-    # formatted_bot_response = {"role": "assistant", "content": f"{bot_response[0]['generated_text']}"}
-    # print(formatted_bot_response[0]['generated_text'])
-    # save_messages(user_message, formatted_bot_response)
+    pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM-360M-Instruct", device=0)
+    parsed_bot_response = pipe(messages, max_new_tokens=128)[0]['generated_text'][-1]
+    save_messages(user_message, parsed_bot_response)
 
 
 # Helper functions
@@ -49,7 +44,7 @@ def load_messages():
                 "role": "system",
                 "content":"You are interviewing the user for a software engineering internship position. "+
                 "Ask short questions that are relevant to a intern level developer. Your name is Greg. "+
-                "The user is Daniel. Keep responses under 30 words and be funny sometimes."
+                "The user is Daniel. Keep responses under 30 words and be funny sometimes, but keep it moving."
             }
         )
     return messages
